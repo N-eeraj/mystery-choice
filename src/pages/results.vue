e149114df10294eca2b9481b8fbb1eb5bac7391c
N-eeraj <neeraj7rajeena@gmail.com>
2023-06-01 13:34:12 +0530

    Show one event at a time

--- src/pages/results.vue
+++ src/pages/results.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 import Choice from '@components/choice/index.vue'
@@ -9,8 +9,14 @@ import { decode } from '@/composables/query'
 const route = useRoute()
 
 const choiceGroups = ref([])
+const currentEvent = ref(0)
 
 const getTheme = index => index % 2 ? 'pink' : 'blue'
+
+const currentChoiceGroup = computed(() => choiceGroups.value[currentEvent.value])
+const showNext = computed(() => choiceGroups.value.length - 1 !== currentEvent.value)
+
+const nextEvent = () => currentEvent.value++
 
 onMounted(() => {
     choiceGroups.value = decode(route.query.choices)
@@ -24,23 +30,31 @@ onMounted(() => {
         </h1>
 
         <div
-            v-for="({event, choices, selected}, index) in choiceGroups"
-            class="w-full">
+            v-if="currentChoiceGroup"
+            class="flex flex-col justify-center items-start gap-y-5 flex-1 w-full">
 
-            <strong>
-                {{ event }}
+            <strong class="text-3xl">
+                {{ currentChoiceGroup.event }}
             </strong>
 
-            <div class="flex flex-col md:flex-row items-center gap-3 flex-1 w-full">
+            <div class="flex flex-col md:flex-row items-center gap-3 w-full">
                 <Choice
-                    v-for="({id, text}, subIndex) in choices"
+                    v-for="({id, text}, subIndex) in currentChoiceGroup.choices"
                     :text="text"
-                    :theme="getTheme(index)"
+                    :theme="getTheme(currentEvent)"
                     reveal
                     read-only
-                    :not-selected="subIndex !== selected"
+                    :not-selected="subIndex !== currentChoiceGroup.selected"
+                    :class="{ 'animate-pulse' : subIndex !== currentChoiceGroup.selected }"
                     :key="id" />
             </div>
+
+            <button
+                v-if="showNext"
+                class="self-end py-1 px-6 bg-white text-black rounded-3xl border-2 border-black duration-300 hover:bg-black hover:text-white"
+                @click="nextEvent">
+                Next
+            </button>
         </div>
     </div>
 </template>
