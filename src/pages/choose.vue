55d028fc6c41bb8d6ef3b92df039142841b6245a
N-eeraj <neeraj7rajeena@gmail.com>
2023-06-01 10:25:16 +0530

    Get choices from query params & list choices

--- src/pages/choose.vue
+++ src/pages/choose.vue
@@ -1,5 +1,36 @@
+<script setup>
+import { computed, onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+import Choice from '@components/choice/index.vue'
+
+const route = useRoute()
+
+const choiceGroups = ref([])
+const currentChoiceGroup = ref(0)
+
+const currentChoices = computed(() => {
+    if (!choiceGroups.value.length)
+        return null
+    return choiceGroups.value[currentChoiceGroup.value].choices
+})
+
+const getTheme = () => currentChoiceGroup.value % 2 ? 'blue' : 'pink'
+
+onMounted(() => {
+    choiceGroups.value = JSON.parse(atob(route.query.choices))
+})
+</script>
+
 <template>
     <h1 class="text-sky-500 text-7xl">
         Choose
     </h1>
+
+    <Choice
+        v-for="({id, text}, index) in currentChoices"
+        :text="text"
+        :theme="getTheme()"
+        read-only
+        :key="id" />
 </template>
